e9cc61259862be912d042967561b310ea1e9e01b
NicoGonzz <juannicolas28@live.com>
2023-07-31 10:32:45 -0500

    Se agregan a pantalla las preguntas con su respuesta , sus funciones y su llamado

--- src/app/components/dashboard-c/dashboard-c.component.ts
+++ src/app/components/dashboard-c/dashboard-c.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import * as XLSX from 'xlsx';
 
+interface CityState { //Interfaz para encontrar estado con mayor acumulado y menor acumulado
+  city: string;
+  state: string;
+}
 
 @Component({
   selector: 'app-dashboard-c',
@@ -11,7 +15,8 @@ export class DashboardCComponent {
   fileChoosen: File | null = null;
   parsedData: any[] = [];
   ExcelData:any[] = []; //Guardamos los datos del excel
-
+  leastRepeatedData: CityState | undefined;
+  mostRepeatedData: CityState | undefined;
 
 
   choosenFile(event: Event) {
@@ -21,18 +26,20 @@ export class DashboardCComponent {
       this.ReadExcel(event);
     }
   }
-
+//Guarda datos
 ReadExcel(event:any){
         let file = event.target.files[0];
 
         let fileReader = new FileReader();
-        fileReader.readAsBinaryString(file);
+        fileReader.readAsBinaryString(file); //Convertimos todos los datos del .csv a binarios
 
         fileReader.onload = (e) =>{
           var workBook = XLSX.read(fileReader.result,{type:'binary'});
           var sheetNames = workBook.SheetNames;
-          this.ExcelData = XLSX.utils.sheet_to_json(workBook.Sheets[sheetNames[0]])
-          console.log(this.ExcelData)
+          this.ExcelData = XLSX.utils.sheet_to_json(workBook.Sheets[sheetNames[0]]); //Convertimmos los datos a JSON
+          console.log(this.ExcelData);
+          this.findLeastRepeatedData();
+          this.findMostRepeatedData();
         }
    }
 
@@ -43,6 +50,61 @@ ReadExcel(event:any){
       fileInput.value = '';
     }
   }
+
+  findLeastRepeatedData() {
+    if (this.ExcelData.length > 0) {   //Confimamos que tenga datos
+      const occurrencesMap: Map<string, number> = new Map(); //Se almaacenan las combinaciones estado-ciudad
+
+      this.ExcelData.forEach((row) => { //lee cada fila
+        const key = `${row['Province_State']}-${row['Admin']}`; //cada nombre se vuelve unico
+        occurrencesMap.set(key, (occurrencesMap.get(key) || 0) + 1);
+      });
+
+      let minOccurrences = Number.MAX_VALUE; //Empezamos por el estado que mas se repita y vamos comparando con los anteriores
+      let leastRepeatedKey = '';
+
+      occurrencesMap.forEach((count, key) => {
+        if (count < minOccurrences) {  //se comparan valores para ver si es mayor o menor
+          minOccurrences = count; //Si es mayor se actualiza
+          leastRepeatedKey = key;
+        }
+      });
+
+      if (leastRepeatedKey) {
+        const [state, city] = leastRepeatedKey.split('-');//Se separa cada dato
+        this.leastRepeatedData = { state, city }; //Se asigna el nuevo valor
+      }
+    }
+  }
+
+
+  findMostRepeatedData() {
+    if (this.ExcelData.length > 0) {
+      const occurrencesMap: Map<string, number> = new Map();
+
+      this.ExcelData.forEach((row) => {
+        const key = `${row['Province_State']}-${row['Admin2']}`;
+        occurrencesMap.set(key, (occurrencesMap.get(key) || 0) + 1);
+      });
+
+      let maxOccurrences = Number.MIN_VALUE;
+      let mostRepeatedKey = '';
+
+      occurrencesMap.forEach((count, key) => {
+        if (count > maxOccurrences) {
+          maxOccurrences = count;
+          mostRepeatedKey = key;
+        }
+      });
+
+      if (mostRepeatedKey) {
+        const [state, city] = mostRepeatedKey.split('-');
+        this.mostRepeatedData = { state, city };
+      }
+    }
+  }
+
+
 
 
 
