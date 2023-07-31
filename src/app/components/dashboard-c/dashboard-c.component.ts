0dbf9eb168c65e5f27947af2c06c57c58ac55ce4
NicoGonzz <juannicolas28@live.com>
2023-07-31 08:39:42 -0500

    Se crea metodo para guardar la Excel Data y mostrar la cantidad de datos

--- src/app/components/dashboard-c/dashboard-c.component.ts
+++ src/app/components/dashboard-c/dashboard-c.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
+
 
 @Component({
   selector: 'app-dashboard-c',
@@ -8,13 +10,31 @@ import { Component } from '@angular/core';
 export class DashboardCComponent {
   fileChoosen: File | null = null;
   parsedData: any[] = [];
+  ExcelData:any[] = []; //Guardamos los datos del excel
+
+
 
   choosenFile(event: Event) {
     const inputElement = event.target as HTMLInputElement;
     if (inputElement.files && inputElement.files.length > 0) {
       this.fileChoosen = inputElement.files[0];
+      this.ReadExcel(event);
     }
   }
+
+ReadExcel(event:any){
+        let file = event.target.files[0];
+
+        let fileReader = new FileReader();
+        fileReader.readAsBinaryString(file);
+
+        fileReader.onload = (e) =>{
+          var workBook = XLSX.read(fileReader.result,{type:'binary'});
+          var sheetNames = workBook.SheetNames;
+          this.ExcelData = XLSX.utils.sheet_to_json(workBook.Sheets[sheetNames[0]])
+          console.log(this.ExcelData)
+        }
+   }
 
   clearFile() {
     this.fileChoosen = null;
@@ -23,6 +43,8 @@ export class DashboardCComponent {
       fileInput.value = '';
     }
   }
+
+
 
 
 }
